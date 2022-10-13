5f2f3c3c19ef901957516b08649bfa1fda601d77
Azib <Azib@IMRANSAEED>
2022-10-13 15:39:03 +0500

    Idd Section styles and admin layout style fixing

--- QRSCS/App_Start/BundleConfig.cs
+++ QRSCS/App_Start/BundleConfig.cs
@@ -6,35 +6,38 @@ namespace QRSCS
     public class BundleConfig
     {
 
-    public static void RegisterBundles(BundleCollection bundles)
+        public static void RegisterBundles(BundleCollection bundles)
         {
-    bundles.Add(new StyleBundle("~/bundles/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
 
-    "~/Content/adminassets/assets/css/assets.css",
-    "~/Content/adminassets/assets/css/typography.css",
-    "~/Content/adminassets/assets/css/shortcodes/shortcodes.css",
-    "~/Content/assetstable/css/sb-admin-2.min.css",
-    "~/Content/assetstable/vendor/fontawesome-free/css/all.min.css",
-     "~/Content/assetstable/vendor/datatables/dataTables.bootstrap4.min.css",
-     "~/Content/adminassets/assets/css/style.css",
-     "~/Content/adminassets/scrollbar.css",
-     "~/Content/adminassets/assets/css/dashboard.css",
-    "~/Content/adminassets/assets/css/color/color-1.css"
-                ));
+            "~/Content/adminassets/assets/css/assets.css",
+            "~/Content/adminassets/assets/css/typography.css",
+            "~/Content/adminassets/assets/css/shortcodes/shortcodes.css",
+            "~/Content/assetstable/css/sb-admin-2.min.css",
+            "~/Content/assetstable/vendor/fontawesome-free/css/all.min.css",
+            "~/Content/assetstable/vendor/datatables/dataTables.bootstrap4.min.css",
+            "~/Content/adminassets/assets/css/style.css",
+            "~/Content/adminassets/scrollbar.css",
+            "~/Content/adminassets/assets/css/dashboard.css",
+            "~/Content/adminassets/assets/css/color/color-1.css",
+            // after place this css idd perfomance background changed from white to grey
+            "~/Content/adminassets/css/style.css"
+
+                        ));
 
 
             // javascript files
-    bundles.Add(new StyleBundle("~/bundles/js").Include(
-   "~/Content/adminassets/assets/vendors/JotForm/jquery.min.js" ,
-   "~/Content/adminassets/assets/js/jquery.min.js" ,
-   "~/Content/adminassets/assets/vendors/owl-carousel/owl.carousel.js",
-   "~/Content/adminassets/assets/js/admin.js" ,
-   "~/Content/adminassets/assets/vendors/counter/counterup.min.js" ,
-   "~/Content/adminassets/assets/js/functions.js" ,
-   "~/Content/adminassets/assets/vendors/bootstrap/js/bootstrap.min.js" ,
-  "~/Content/adminassets/assets/vendors/chart/chart.min.js"
-     ));
-            
+            bundles.Add(new StyleBundle("~/bundles/js").Include(
+           "~/Content/adminassets/assets/vendors/JotForm/jquery.min.js",
+           "~/Content/adminassets/assets/js/jquery.min.js",
+           "~/Content/adminassets/assets/vendors/owl-carousel/owl.carousel.js",
+           "~/Content/adminassets/assets/js/admin.js",
+           "~/Content/adminassets/assets/vendors/counter/counterup.min.js",
+           "~/Content/adminassets/assets/js/functions.js",
+           "~/Content/adminassets/assets/vendors/bootstrap/js/bootstrap.min.js",
+          "~/Content/adminassets/assets/vendors/chart/chart.min.js"
+             ));
+
         }
     }
 }
