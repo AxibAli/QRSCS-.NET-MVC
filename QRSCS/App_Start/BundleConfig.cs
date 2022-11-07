d9c33fee94c42e8b8c72c723b7362f482f2501d1
Azib <Azib@IMRANSAEED>
2022-11-07 11:37:57 +0500

    create user profile pic work

--- QRSCS/App_Start/BundleConfig.cs
+++ QRSCS/App_Start/BundleConfig.cs
@@ -14,8 +14,8 @@ namespace QRSCS
             "~/Content/adminassets/assets/css/typography.css",
             "~/Content/adminassets/assets/css/shortcodes/shortcodes.css",
             "~/Content/assetstable/css/sb-admin-2.min.css",
-            "~/Content/assetstable/vendor/fontawesome-free/css/all.min.css",
             "~/Content/assetstable/vendor/datatables/dataTables.bootstrap4.min.css",
+            "~/Content/assetstable/vendor/fontawesome-free/css/all.min.css",
             "~/Content/adminassets/assets/css/style.css",
             "~/Content/adminassets/scrollbar.css",
             "~/Content/adminassets/assets/css/dashboard.css",
@@ -28,14 +28,14 @@ namespace QRSCS
 
             // javascript files
             bundles.Add(new StyleBundle("~/bundles/js").Include(
+           "~/Content/adminassets/assets/vendors/bootstrap/js/bootstrap.min.js",
            "~/Content/adminassets/assets/vendors/JotForm/jquery.min.js",
            "~/Content/adminassets/assets/js/jquery.min.js",
            "~/Content/adminassets/assets/vendors/owl-carousel/owl.carousel.js",
            "~/Content/adminassets/assets/js/admin.js",
            "~/Content/adminassets/assets/vendors/counter/counterup.min.js",
            "~/Content/adminassets/assets/js/functions.js",
-           "~/Content/adminassets/assets/vendors/bootstrap/js/bootstrap.min.js",
-          "~/Content/adminassets/assets/vendors/chart/chart.min.js"
+           "~/Content/adminassets/assets/vendors/chart/chart.min.js"
              ));
 
         }
